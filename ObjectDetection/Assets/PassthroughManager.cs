42027feafd315471b4f47e83a87510b5860eca12
marlonfabpereira <marlon.fa.pereira@hotmail.com>
2024-06-22 18:06:20 -0400

    functional passthrough toggle

--- ObjectDetection/Assets/PassthroughManager.cs
+++ ObjectDetection/Assets/PassthroughManager.cs
@@ -6,23 +6,28 @@ using TMPro;
 public class PassthroughManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public OVRPassthroughLayer passthroughLayer;
+    public Canvas canvas;
+    public MeshRenderer meshRenderer;
 
     public TextMeshPro textMesh;
+    private bool isPassthrough = false;
 
     void Start()
     {
-        
+        canvas.enabled = isPassthrough;
+        meshRenderer.enabled = isPassthrough;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Three) || OVRInput.GetDown(OVRInput.Button.Four) ) 
+        if (OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Three) || OVRInput.GetDown(OVRInput.Button.Four))
         {
+            isPassthrough = !isPassthrough;
             Debug.Log("Toggling Passthrough");
-            textMesh.text = passthroughLayer.overlayType == OVROverlay.OverlayType.Underlay ? "Passthrough: OFF" : "Passthrough: ON";
-            passthroughLayer.overlayType = passthroughLayer.overlayType == OVROverlay.OverlayType.Underlay ? OVROverlay.OverlayType.None : OVROverlay.OverlayType.Underlay;
+            textMesh.text = isPassthrough ? "Passthrough: ON" : "Passthrough: OFF";
+            canvas.enabled = isPassthrough;
+            meshRenderer.enabled = isPassthrough;
         }
     }
 }
