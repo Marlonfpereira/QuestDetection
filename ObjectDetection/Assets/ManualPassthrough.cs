f0e72950f3811603177d76adb10f702ecb9a4b87
marlonfabpereira <marlon.fa.pereira@hotmail.com>
2024-07-04 17:40:51 -0400

    removing object detection

--- ObjectDetection/Assets/ManualPassthrough.cs
+++ ObjectDetection/Assets/ManualPassthrough.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.MeshOperations;
-using UnityEngine.Networking;
-using Newtonsoft.Json;
 
 public class ManualPassthrough : MonoBehaviour
 {
@@ -12,8 +10,6 @@ public class ManualPassthrough : MonoBehaviour
     public Canvas canvas;
     public MeshRenderer meshRenderer;
     private bool isPassthrough = false;
-
-    public Camera mainCamera;
     public GameObject objectLoader;
     private GameObject meshesLoader;
     public Material passthroughMaterial;
@@ -22,13 +18,9 @@ public class ManualPassthrough : MonoBehaviour
     private bool triggerPressed = false; 
     private List<GameObject> currentSet = new List<GameObject>();
 
-    private GameObject autoLoader;
-    private Ray ray1, ray2, ray3, ray4;
-
     void Start()
     {
         meshesLoader = new GameObject("MeshesLoader");
-        autoLoader = new GameObject("AutoLoader");
         canvas.enabled = isPassthrough;
         meshRenderer.enabled = isPassthrough;
     }
@@ -43,19 +35,6 @@ public class ManualPassthrough : MonoBehaviour
 
             canvas.enabled = isPassthrough;
             meshRenderer.enabled = isPassthrough;
-        }
-
-        if (isPassthrough && OVRInput.GetDown(OVRInput.Button.Three))
-        {
-            Debug.Log("Getting data from API");
-            StartCoroutine(GetDataFromAPI());
-        }
-
-        if (OVRInput.GetDown(OVRInput.Button.Four)) {
-            foreach (Transform child in autoLoader.transform)
-            {
-                GameObject.Destroy(child.gameObject);
-            }
         }
 
         if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger) && !triggerPressed)
@@ -110,51 +89,4 @@ public class ManualPassthrough : MonoBehaviour
 
         testSphere.transform.position = controllerPos;
     }
-
-    IEnumerator GetDataFromAPI()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get("http://192.168.137.1/centerPrediction"))
-        {
-            Debug.Log("API called");
-            yield return www.SendWebRequest();
-
-            if (www.result == UnityWebRequest.Result.Success)
-            {
-                string response = www.downloadHandler.text;
-
-                DetectedObject detectedObject = JsonConvert.DeserializeObject<DetectedObject>(response);
-
-                foreach (Transform child in autoLoader.transform)
-                {
-                    GameObject.Destroy(child.gameObject);
-                }
-
-                float x1 = detectedObject.x1 * Screen.width;
-                float x2 = detectedObject.x2 * Screen.width;
-                float y1 = ((detectedObject.y1 * -1) + 1) * Screen.height;
-                float y2 = ((detectedObject.y2 * -1) + 1) * Screen.height;
-
-                ray1 = mainCamera.ScreenPointToRay(new Vector3(x1, y1, 0));
-                ray2 = mainCamera.ScreenPointToRay(new Vector3(x2, y1, 0));
-                ray3 = mainCamera.ScreenPointToRay(new Vector3(x1, y2, 0));
-                ray4 = mainCamera.ScreenPointToRay(new Vector3(x2, y2, 0));
-
-                RaycastHit hit1, hit2, hit3, hit4;
-                if (Physics.Raycast(ray1, out hit1, 100) && Physics.Raycast(ray2, out hit2, 100) && Physics.Raycast(ray3, out hit3, 100) && Physics.Raycast(ray4, out hit4, 100))
-                {
-                    Mesh mesh = new Mesh();
-                    mesh.vertices = new Vector3[] { hit1.point, hit2.point, hit3.point, hit4.point };
-                    mesh.triangles = new int[] { 0, 1, 2, 3, 2, 1 };
-
-                    GameObject meshObject = new GameObject("Mesh");
-                    meshObject.AddComponent<MeshFilter>().mesh = mesh;
-                    meshObject.AddComponent<MeshRenderer>();
-                    meshObject.GetComponent<MeshRenderer>().material = passthroughMaterial;
-                    meshObject.transform.parent = autoLoader.transform;
-                }
-
-            }
-        }
-    }
-
 }
