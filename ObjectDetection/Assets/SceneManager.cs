fe91faa8a189a7c6fc5840dc9b98a0f833640aca
marlonfabpereira <marlon.fa.pereira@hotmail.com>
2024-08-09 09:15:07 -0400

    keyboard interactions

--- ObjectDetection/Assets/SceneManager.cs
+++ ObjectDetection/Assets/SceneManager.cs
@@ -13,29 +13,53 @@ public class SceneManager : MonoBehaviour
     private int currentStep = -1;
     void Start()
     {
-        plane1.SetActive(true);;
+        plane1.SetActive(true); ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             currentStep++;
             Debug.Log(currentStep);
             ManageSteps();
         }
-        else if (Input.GetMouseButtonDown(1))
+        else if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             currentStep--;
             Debug.Log(currentStep);
             ManageSteps();
         }
-        else if (Input.GetMouseButton(2))
+        else if (Input.GetMouseButton(2) || Input.GetKeyDown(KeyCode.Backspace))
         {
             //reset
             currentStep = -1;
             Debug.Log(currentStep);
+            ManageSteps();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            currentStep = 4;
+            ManageSteps();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            currentStep = 6;
+            ManageSteps();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            currentStep = 15;
+            ManageSteps();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            currentStep = 17;
+            ManageSteps();
+        } else if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            currentStep = 19;
             ManageSteps();
         }
     }
@@ -54,10 +78,10 @@ public class SceneManager : MonoBehaviour
         // {
         //     StartCoroutine(audioManager.WaitAndPlayNextClip());
         // }
-        
+
         audioManager.StopAllCoroutines();
         audioManager.PlayClip(currentStep);
 
-        plane1.GetComponent<MeshRenderer>().material = materials[currentStep%20];
+        plane1.GetComponent<MeshRenderer>().material = materials[currentStep % 20];
     }
 }
